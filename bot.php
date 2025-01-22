c25b6e58fc9a8bd5ab6a20b454ba2546d08758c7
pureabc <136778708+pureabc@users.noreply.github.com>
2025-01-22 17:39:01 +0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7512082512:AAHdk7MrpDy4CN6IsKOyfrsqcCEL92zkcHA";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
